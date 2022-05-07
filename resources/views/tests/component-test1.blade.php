bf3d670e03db620e0975e1e5bfeabb39f8d5a620
rikupoo <nemutir@gmail.com>
2022-05-07 11:20:59 +0900

    TestComponent

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -3,4 +3,7 @@
     ヘッダ1
   </x-slot>
 コンポーネントテスト１
+  <x-tests.card title="タイトル" content="本文" :message="$message" />
+  <x-tests.card title="タイトル2" />
+  <x-tests.card title="CSS変更" class="bg-red-300" />
 </x-tests.app>
--- resources/views/tests/component-test2.blade.php
+++ resources/views/tests/component-test2.blade.php
@@ -3,4 +3,6 @@
     ヘッダ2
   </x-slot>
 コンポーネントテスト2
+  <x-test-class-base classBaseMessage="メッセージ表示"/>
+  <x-test-class-base classBaseMessage="メッセージ表示2" defaultMessage="初期値から変更"/>
 </x-tests.app>
