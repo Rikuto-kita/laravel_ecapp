050a665d0cd9f9b8dbf6da916c8c1a4cd5e36320
rikupoo <nemutir@gmail.com>
2022-05-14 22:34:51 +0900

    ImageStore

--- app/Services/ImageService.php
+++ app/Services/ImageService.php
@@ -7,11 +7,15 @@ use InterventionImage;
 
 class ImageService
 {
-  public static function upload($imageFile, $folderName)
-  {
-    $resizedImage = InterventionImage::make($imageFile)->resize(1920, 1080)->encode();
+  public static function upload($imageFile, $folderName){
+    if(is_array($imageFile)){
+      $file = $imageFile['image']; // 配列なので[‘key’] で取得
+    }else{
+      $file = $imageFile;//配列でなければ$imageFileに入れる
+    }
+    $resizedImage = InterventionImage::make($file)->resize(1920, 1080)->encode();
     $fileName = uniqid(rand().'_');//ランダムのファイル名を作成
-    $extension = $imageFile->extension();//拡張子の取得
+    $extension = $file->extension();//拡張子の取得
     $fileNameToStore = $fileName. '.' . $extension;//ファイル名拡張子をつける
     Storage::put('public/'.$folderName.'/'.$fileNameToStore, $resizedImage );
 
