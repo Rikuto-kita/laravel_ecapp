4f23d8ad53314ea64a842e8f624f19f09aa8cb8a
rikupoo <nemutir@gmail.com>
2022-05-15 10:33:05 +0900

    EagerLoading

--- app/Models/Shop.php
+++ app/Models/Shop.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Owner;
+use App\Models\Product;
 
 class Shop extends Model
 {
@@ -19,9 +20,14 @@ class Shop extends Model
     ];
 
 
-    public function Owner()
+    public function owner()
     {
         return $this->belongsTo(Owner::class);
+    }
+
+    public function product()
+    {
+        return $this->hasMany(Product::class);
     }
 
 }
