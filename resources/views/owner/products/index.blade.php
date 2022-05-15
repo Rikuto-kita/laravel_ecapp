29cca540bd5345ff07552a6244af608f3fc9970a
rikupoo <nemutir@gmail.com>
2022-05-15 19:00:53 +0900

    ownerInfo

--- resources/views/owner/products/index.blade.php
+++ resources/views/owner/products/index.blade.php
@@ -14,7 +14,7 @@
                   <button onclick="location.href='{{ route('owner.products.create') }}'" class="text-white bg-red-500 border-0 py-2 px-8 focus:outline-none hover:bg-red-600 rounded text-lg">新規登録する</button>
                 </div>
                 <div class="flex flex-wrap">
-                    @foreach ($ownerInfo as $owner)
+                   @foreach ($ownerInfo as $owner )
                       @foreach($owner->shop->product as $product)
                     <div class="w-1/4 p-2 md:p-4">
                       <a href="{{ route('owner.products.edit' , ['product' => $product -> id]) }}">
