7067a6b0d187b10e6e0508317758803b310ed289
rikupoo <nemutir@gmail.com>
2022-05-21 13:28:20 +0900

    productUpdate

--- app/Http/Controllers/Owner/ProductController.php
+++ app/Http/Controllers/Owner/ProductController.php
@@ -12,6 +12,7 @@ use App\Models\Owner;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB; 
 use App\Models\Stock;
+use App\Http\Requests\ProductRequest;
 
 class ProductController extends Controller
 {
@@ -20,6 +21,7 @@ class ProductController extends Controller
         $this->middleware('auth:owners');
 
         $this->middleware(function ($request, $next) {
+
             $id = $request->route()->parameter('product'); 
                 if(!is_null($id)){ // null判定
                     $productsOwnerId = Product::findOrFail($id)->shop->owner->id;
@@ -74,7 +76,7 @@ class ProductController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(ProductRequest $request)
     {
         $request->validate([
             'name' => ['required', 'string', 'max:50'],
@@ -144,16 +146,64 @@ class ProductController extends Controller
         return view('owner.products.edit',compact('product','quantity','shops','images','categories'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
+    
+    public function update(ProductRequest $request, $id)
     {
-        //
+        $request->validate([
+            'current_quantity' => ['required', 'integer'],
+        ]);
+
+        $product = Product::findOrFail($id);
+        $quantity = Stock::where('product_id', $product->id)
+        ->sum('quantity');
+
+        if($request->current_quantity !== $quantity){
+
+            $id = $request->route()->parameter('product'); 
+            return redirect()->route('owner.products.edit',['product' => $id])
+            ->with(['message'=>'在庫数が変更されています、再度確認してください。',
+            'status'=>'alert']);
+        }else{
+
+            try{
+                DB::transaction(function() use($request,$product){
+                  
+                        $product->name = $request->name;
+                        $product->information = $request->information;
+                        $product->price = $request->price;
+                        $product->sort_order = $request->sort_order;
+                        $product->shop_id = $request->shop_id;
+                        $product->secondary_category_id = $request->category;
+                        $product->image1 = $request->image1;
+                        $product->image2 = $request->image2;
+                        $product->image3 = $request->image3;
+                        $product->image4 = $request->image4;
+                        $product->is_selling = $request->is_selling;
+                        $product->save();
+                    
+    if($request->type === '1'){
+        $newQuantity = $request->quantity;
+    }
+    if($request->type === '2'){
+        $newQuantity = $request->quantity * -1;
+    }
+                    Stock::create([
+                        'product_id' => $product->id,
+                        'type' => $request->type,
+                        'quantity' => $newQuantity,
+                    ]);
+                },2);
+            }catch(Throwable $e){
+                Log::error($e);
+                throw $e;
+            }
+    
+            return redirect()->route('owner.products.index')
+            ->with(['message'=>'商品情報を更新しました。',
+            'status'=>'info']);
+
+        }
+
     }
 
     /**
