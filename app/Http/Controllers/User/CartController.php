b26f6aabdf5a54693aab7157f3f89956be191c65
rikupoo <nemutir@gmail.com>
2022-05-22 16:13:17 +0900

    stripeCancel

--- app/Http/Controllers/User/CartController.php
+++ app/Http/Controllers/User/CartController.php
@@ -92,7 +92,7 @@ class CartController extends Controller
              'line_items' => [$lineItems],
              'mode' => 'payment',
              'success_url' => route('user.cart.success'),
-             'cancel_url' => route('user.cart.index'),
+             'cancel_url' => route('user.cart.cancel'),
         ]);
 
         $publicKey = env('STRIPE_PUBLIC_KEY');
@@ -102,8 +102,23 @@ class CartController extends Controller
 
     public function success()
     {
-Cart::where('user_id',Auth::id())->delete();
+        Cart::where('user_id',Auth::id())->delete();
 
-return redirect()->route('user.items.index');
+        return redirect()->route('user.items.index');
+    }
+
+    public function cancel()
+    {
+        $user = User::findOrFail(Auth::id());
+
+        foreach($user->products as $product) {
+            Stock::create([
+            'product_id' => $product->id,
+            'type' => \Constant::PRODUCT_LIST['add'],
+            'quantity' => $product->pivot->quantity
+            ]);
+        }
+
+        return redirect()->route('user.cart.index');
     }
 }
