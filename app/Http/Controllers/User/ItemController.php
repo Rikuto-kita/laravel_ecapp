43b4eadece4947d1e9fb64ec88afa1f9906a0775
rikupoo <nemutir@gmail.com>
2022-05-23 20:20:34 +0900

    Mail

--- app/Http/Controllers/User/ItemController.php
+++ app/Http/Controllers/User/ItemController.php
@@ -8,6 +8,8 @@ use App\Models\Product;
 use App\Models\Stock;
 use Illuminate\Support\Facades\DB;
 use App\Models\PrimaryCategory;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\TestMail;
 
 class ItemController extends Controller
 {
@@ -32,6 +34,10 @@ class ItemController extends Controller
 
     public function index(Request $request)
     {
+
+        Mail::to('test@testtest.com')//受信者の指定
+        ->send(new TestMail);//Mailableクラス
+        
         $categories = PrimaryCategory::with('secondary')
         ->get();
 
